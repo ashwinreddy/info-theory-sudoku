1eea1f503271ff841a6c57607279ded80086a40a
Ashwin Reddy <ashwin.reddy338@gmail.com>
2018-12-15 16:19:36 -0800

    comments explaining what needs to be done for rough draft

---
+++ __main__.py
@@ -0,0 +1,13 @@
+# Determines a sudoku grid by asking the user questions
+
+# This class will keep track of all the known and unknown cells
+class SudokuGrid(object):
+    pass
+
+
+if __name__ == "__main__":
+    while True:
+        # ask user a question
+        # record information in grid
+        # repeat
+        pass
