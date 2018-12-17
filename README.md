2bc0d2f06a461facb5e11df230fbb00cf0ec069d
Ashwin Reddy <ashwin.reddy338@gmail.com>
2018-12-16 17:00:11 -0800

    added new grid test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Info Theory Sudoku Project
 
+Test file 1 came from http://www.7sudoku.com/view-puzzle?date=20181211
+Test file 2 came from http://www.7sudoku.com/view-puzzle?date=20181205
--- __main__.py
+++ __main__.py
@@ -4,17 +4,18 @@ from grid import SudokuGrid, Interrogator
 import logging
 logging.basicConfig(level=logging.CRITICAL)
 
-grid = np.loadtxt("test_grids/1.txt", delimiter=' ', dtype='int8')
+grid = np.loadtxt("test_grids/2.txt", delimiter=' ', dtype='int8')
 interrogator = Interrogator(False)
 interrogator.set_grid(grid)
 sg = SudokuGrid(interrogator)
 
-# np.unravel_index()
 flat_idx = 0
 
 while not sg.completed:
     i, j = np.unravel_index(flat_idx, grid.shape)
     sg.determine_cell(i, j, sg.viable_indices(i, j))
     flat_idx += 1
-    # input("?")
+    print(sg)
+    input("?")
+    
 print(interrogator.questions_asked)
